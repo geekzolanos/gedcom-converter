32a228192e30e049790d3021627aa4a332faba56
David Romero <geekzolanos@gmail.com>
2016-09-12 23:16:05 -0500

    Migrada plantilla Base a sistema de Templates.
    Se ha migrado todos los metodos responsables de generar el codigo HTML a partir de la informacion de los nodos, al nuevo esquema de Templates.
    La gestion del indice actual, la entrega del nodo y la escritura del documento en el disco seguiran siendo trabajo del GenManager.
    Los templates, solo incluyen metodos para generar un String con la sintaxis HTML a partir del objeto Node que se les envia como parametro.

--- src/scripts/gen-manager.js
+++ src/scripts/gen-manager.js
@@ -5,6 +5,7 @@
 
 window.GenManager = function() {
     // Propiedades privadas
+    let currentTemplate = Templates.get('Base');
     let parsedData = {};
     let parsedKeys = [];
 
@@ -23,153 +24,14 @@ window.GenManager = function() {
     // Metodos privados
 
     let generateNext = () => {
-        let idxNode = parseInt(window.sessionStorage.getItem(ssURI.currentNode));
+        let idxNode = parseInt(window.sessionStorage.getItem(ssURI.currentNode));       
+        let dirpath = window.sessionStorage.getItem(ssURI.dirPath);
         console.log('Nodo Actual: ' + idxNode);
-        this.generateHTML(parsedData[parsedKeys[idxNode]]);
+        let content = this.generateHTML(parsedData[parsedKeys[idxNode]]);
+        // Escribimos los datos
+        fs.writeFile(dirpath + '/' + parsedKeys[idxNode] + '.html', content, generateNext);
         window.sessionStorage.setItem(ssURI.currentNode, idxNode + 1);
         app.ui.utils.convUpdateProgress();
-    }
-
-    let generateHTMLAsPerson = (node) => {
-        let HTMLTree = [];
-        let dirpath = window.sessionStorage.getItem(ssURI.dirPath);
-        let currentTime = new Date().getTime();
-        let name = node.plugin.name.join("");
-        let birthYear = (node.plugin.birt) ? node.plugin.birt.date : "Unknown Birth Year";
-        let locationBirth = (node.plugin.birt) ? node.plugin.birt.place : "Unknown location";
-        let sex = node.plugin.sexe || "Unknown";
-
-        HTMLTree.push(HTMLHeadStart);
-        /* 
-            birth: Object
-            deat: Object
-            familleParent: klass
-            familles: Array[2]
-            name: Array[2]
-            privacy: undefined
-            sexe: "M"
-        */
-        HTMLTree.push([
-            '<title>' + '' + '</title>',
-            '<meta name="Title" content="' + name + '"></meta>',
-            '<meta name="BirthYear" content="' + birthYear + '"></meta>',
-            '<meta name="CurrentDate" content="' + currentTime + '"></meta>',
-            '<meta name="Description" content="' + name + " - " + locationBirth + '"></meta>',
-        ].join(''));
-
-        HTMLTree.push(HTMLHeadEnd);
-        HTMLTree.push(HTMLBodyStart);
-
-        // Nombre
-        HTMLTree.push('<h1>' + name + '</h1>');
-        HTMLTree.push('<br>');
-
-        // Fecha de Natalidad
-        HTMLTree.push([
-            '<h2>Birth Info</h2>',
-            '<h4>Year</h4>',
-            '<p>' + birthYear + '</p>',
-            '<h4>Location</h4>',
-            '<p>' + locationBirth + '</p>'
-        ].join(''));
-        HTMLTree.push('<br><br>');
-
-        // Fecha de Mortalidad
-        if(node.plugin.deat) {
-            let deatYear = node.plugin.deat.date;
-            let deatBirth = node.plugin.deat.place || "Unknown location";
-
-            HTMLTree.push([
-                '<h2>Death Info</h2>',
-                '<h4>Year</h4>',
-                '<p>' + deatYear + '</p>',
-                '<h4>Location</h4>',
-                '<p>' + deatBirth + '</p>'
-            ].join(''));
-            HTMLTree.push('<br><br>');
-        }
-
-        // Padre
-        if(node.plugin.familleParent) {
-            let fatherNode = node.plugin.familleParent.husb;
-            let motherNode = node.plugin.familleParent.wife;
-
-            HTMLTree.push('<h2>Parent Info</h2>');
-
-            if(fatherNode)
-                HTMLTree.push([
-                    '<h4>Father</h4>',
-                    '<a href="' + fatherNode.id + '.html">' + fatherNode.plugin.name.join('') + '</a>'
-                ].join(''));
-
-            if(motherNode)
-                HTMLTree.push([                    
-                    '<h4>Mother</h4>',
-                    '<a href="' + motherNode.id + '.html">' + motherNode.plugin.name.join('') + '</a>'
-                ].join(''));
-
-            HTMLTree.push('<br><br>');
-        }          
-
-        // Familias
-        if(node.familles) {
-            let conyuges = [];
-            let childrens = [];
-
-            HTMLTree.push("<h2>Familia</h2>")
-            // Recolectamos la informacion de la familia
-            node.familles.forEach((famille) => {
-                // Conyuge
-                let conyuge = (sexe == "M") ? famille.wife : famille.husb;
-                if(conyuge)
-                    conyuges.push(conyuge);
-                // Hijos
-                if (famille.childs) {
-                    famille.childs.forEach((child) => {
-                        childrens.push(child);
-                    });
-                }
-            });
-            if (conyuges.length > 0) {
-                HTMLTree.push("<h3>Conyuges</h4>");
-                conyuges.forEach((conyuge) => {
-                    HTMLTree.push('<a href="' + conyuge.id + '.html">' + conyuge.plugin.name.join('') + '</a>');
-                });
-            }
-            if (childrens.length > 0) {
-                HTMLTree.push("<h3>Hijos</h4>");
-                childrens.forEach((child) => {
-                    HTMLTree.push('<a href="' + child.id + '.html">' + child.plugin.name.join('') + '</a>');
-                });
-            }
-        }
-
-        // Sexo
-        HTMLTree.push("<h2>Sexo</h2>");
-        switch(sex) {
-            case "M":
-                HTMLTree.push('<p>Masculino</p>');
-            break;
-            case "F":
-                HTMLTree.push('<p>Femenino</p>');
-            break;
-            default:
-                HTMLTree.push('<p>Desconocido</p>');
-            break;
-        }
-
-        HTMLTree.push(HTMLBodyEnd);
-
-        // Escribimos los datos
-        fs.writeFile(dirpath + '/' + node.id + '.html', HTMLTree.join(''), generateNext);
-    }
-
-    let generateHTMLAsFamily = (node) => {
-        
-    }
-
-    let generateHTMLAsSource = (node) => {
-        
     }
 
     // Metodos Publicos
@@ -180,18 +42,15 @@ window.GenManager = function() {
         switch(node.type) {
             case GedcomConst.indicator.personne:
                 console.log('Es una Persona!');
-                generateHTMLAsPerson(node);
-            break;
+                return currentTemplate.generatePerson(node);
 
             case GedcomConst.indicator.famille:
                 console.log('Es una Familia!');
-                generateHTMLAsFamily(node);
-            break;
+                return currentTemplate.generateFamily(node);
 
             case GedcomConst.indicator.source:
                 console.log('Es una Fuente!');
-                generateHTMLAsSource(node);
-            break;
+                return currentTemplate.generateSource(node);
         }
     }
 
