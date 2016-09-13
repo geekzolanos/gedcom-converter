b4284a076647d858531cf87059a3fa236c62c191
David Romero <geekzolanos@gmail.com>
2016-09-12 23:07:15 -0500

    Implementacion Inicial de clase Templates.

---
+++ src/scripts/template-manager.js
@@ -0,0 +1,28 @@
+//
+//  Template Manager
+//  Un (Extremadamente) Simple administrador de plantillas
+//
+
+window.Template = function(name, gPerson, gFamily) {
+    this.name = name;
+    this.generatePerson = gPerson;
+    this.generateFamily = gFamily;
+}
+
+// TODO: Llevar esto al esquema de Getter y Setters
+window.Templates = {
+    catalog: {},
+
+    register: (obj) => {
+        if(obj.constructor != Template)
+            throw new TypeError('El objeto no es un Template');
+        Templates.catalog[obj.name] = obj;
+    },
+
+    get: (name) => {
+        let template = Templates.catalog[name];
+        if(!template)
+            throw new ReferenceError('La plantilla no existe.');
+        return template;
+    }
+}
