943c152589084e8412f52845c2e22110a4628cc9
David Romero <geekzolanos@gmail.com>
2016-09-15 17:21:22 -0500

    Implementacion de 'Loader' y Version de app
    Se ha implementado una pantalla de carga en la aplicacion.
    
    Adicionalmente se ha establecido como variable global la version de la aplicacion para facilitar el aumento de la misma.

--- src/app.js
+++ src/app.js
@@ -2,24 +2,49 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
+// App version
+global._VERSION = "1.0.1";
+
+// BrowserWindow Nodes
 let mainWindow = null;
+let splashScreen = null;
 
 function load() {
+    // MainWindow Prefs
     mainWindow = new BrowserWindow({
         width: 750,
         height: 450,
         resizable: false,
         show: false
-    });
-    
+    });    
     mainWindow.setMenu(null);
+
+    // BrowserWindow Path
     mainWindow.loadURL('file://' + __dirname + '/main.html');
+
+    // BrowserWindow Events
     mainWindow.on('ready-to-show', () => {
         mainWindow.show();
+        splashScreen.hide();
     });
+
     mainWindow.on('closed', () => {
         app.quit();
     });
 }
 
-app.on('ready', load);
+function start() {
+    splashScreen = new BrowserWindow({
+        width: 300,
+        height: 150,
+        frame: false,
+        show: false
+    });
+
+    splashScreen.loadURL('file://' + __dirname + '/loader.html');
+    splashScreen.on('ready-to-show', () => {
+        splashScreen.show();
+        load();
+    });
+}
+app.on('ready', start);
