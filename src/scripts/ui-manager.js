1cc71e1ea5cbf2821cf2ecbce9b15eff13494df7
David Romero <geekzolanos@gmail.com>
2016-09-15 23:57:03 -0500

    Integracion de variable global _VERSION en cuadro acerca de.

--- src/scripts/ui-manager.js
+++ src/scripts/ui-manager.js
@@ -55,13 +55,14 @@
             
             // Boton Acerca de...
             let btnAbout = document.querySelector('header .btn-about');
-             btnAbout.addEventListener('click', () => {
+            let version = electron.getGlobal('_VERSION');
+            btnAbout.addEventListener('click', () => {
                 electron.dialog.showMessageBox({
                     type: "info",
                     buttons: [],
                     title: "About...",
                     message: "Gedcom Converter",
-                    detail: "Version 1.0\nDeveloper by Geekzolanos for Upwork.\nFor more information, please contact us sending a e-mail to\n\ngeekzolanos@gmail.com"
+                    detail: "Version " + version + "\nDeveloper by Geekzolanos for Upwork.\nFor more information, please contact us sending a e-mail to\n\ngeekzolanos@gmail.com"
                 });
             });
         },
