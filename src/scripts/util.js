084203e1e87b0b193385f71ad3d54b7aa561f5f9
David Romero <geekzolanos@gmail.com>
2016-09-17 10:23:30 -0500

    Convertidas propiedades de 'utils.js' en constantes

--- src/scripts/util.js
+++ src/scripts/util.js
@@ -3,13 +3,13 @@
 //  Utilerias de la aplicacion
 //
 
-let ErrCode = {
+const ErrCode = {
     GEDCOM_FILE_NOT_EXISTS: 0,
     GEDCOM_FILE_INVALID_PARAMETER: 1,
     GEDCOM_PARSER_UNKNOWN: 2
 } 
 
-let Tones = {
+const Tones = {
     light: {
         BLUE: 1,
         ALPHA_LIGHT: 3,
