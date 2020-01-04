daac1f6af97624829def90fd28ae370f618ca39c
David Romero <geekzolanos@gmail.com>
2020-01-04 09:37:29 -0400

    Integracion de gedcom-js-viewer como submodulo

--- src/scripts/gen-manager.js
+++ src/scripts/gen-manager.js
@@ -2,14 +2,14 @@
 //  Generator Manager
 //  Administrador del proceso de conversion
 //
-
+DEBUG = true;
 window.GenManager = function() {
     // Propiedades privadas
     let currentTemplate = Templates.get('Base');
     let idxCurrentNode = -1;
     let parsedData = {};
     let parsedKeys = [];
-    let directoryData = {persons: [], families: []};    
+    let directoryData = { persons: [], families: [] };
 
     // Metodos Publicos
     let cleanPersonNode = (node) => {
@@ -23,16 +23,16 @@ window.GenManager = function() {
         node.plugin.birt.place = (birthLoc == "undefined") ? undefined : birthLoc;
 
         // Agregamos la propiedad year
-        if(birthDate) {
+        if (birthDate) {
             let by_start = birthDate.length;
             let by_end = birthDate.length - 4;
             let birthYear = parseInt(birthDate.substring(by_start, by_end));
-            if(isNaN(birthYear) === false)
+            if (isNaN(birthYear) === false)
                 node.plugin.birt.year = birthYear.toString();
         }
 
         // La fecha y lugar de mortalidad no puede ser "undefined"
-        if(node.plugin.deat) {
+        if (node.plugin.deat) {
             let deatDate = node.plugin.deat.date;
             let deatLoc = node.plugin.deat.place;
             node.plugin.deat.date = (deatDate == "undefined") ? undefined : deatDate;
@@ -41,13 +41,13 @@ window.GenManager = function() {
 
         // La familia debe ser un array
         let familles = node.plugin.familles;
-        if(Array.isArray(familles) === false)
+        if (Array.isArray(familles) === false)
             node.plugin.familles = undefined;
 
         // La familia no puede ser un array vacio
-        else if(familles.length == 0)
+        else if (familles.length == 0)
             node.plugin.familles = undefined;
-        
+
         else {
             familles.forEach((family, i) => {
                 // Childrens debe ser un array y no puede ser vacio
@@ -57,56 +57,59 @@ window.GenManager = function() {
                 else if (childs.length == 0)
                     familles[i].childs = undefined
             });
-        }  
+        }
 
-        if(DEBUG) {
+        if (!!DEBUG) {
             console.log('Estructura despues de la limpieza del nodo:');
             console.log(node);
-        }      
+        }
     }
 
     let generateContent = (node) => {
-        if(DEBUG) {
+        if (!!DEBUG) {
             console.log('Procesando Nodo');
             console.log(node);
         }
 
         // Es posible que algunos nodos sean indefinidos (Por error del formato) Asi que los ignoramos.
-        if(!node)
+        if (!node)
             return false;
 
-        switch(node.type) {
+        // Fixed ID
+        node.fixedID = node.id.replace(/[\@]/g, "");
+        
+        switch (node.type) {
             case GedcomConst.indicator.personne:
-                if(DEBUG)
+                if (!!DEBUG)
                     console.log('Es una Persona!');
 
                 // Limpiando datos de entrada del nodo
                 cleanPersonNode(node);
 
                 // Colocamos una referencia en el directorio para el indice
-                directoryData.persons.push({id: node.fixedID, value: node.plugin.name.join('\xa0')});
+                directoryData.persons.push({ id: node.fixedID, value: node.plugin.name.join('\xa0') });
 
                 return currentTemplate.generatePerson(node);
 
             case GedcomConst.indicator.famille:
-                if(DEBUG)
+                if (!!DEBUG)
                     console.log('Es una Familia!');
 
-                if(Preferences.session.options.noFamily === true) {
-                    if(DEBUG)
+                if (Preferences.session.options.noFamily === true) {
+                    if (!!DEBUG)
                         console.log('No se permite generar paginas de familias.');
                     return false;
                 }
-                
+
                 // Colocamos una referencia en el directorio para el indice
-                directoryData.families.push({id: node.fixedID, value: (node.husb) ? node.husb.plugin.name[node.husb.plugin.name.length - 1] : "Unknown Family name"});
+                directoryData.families.push({ id: node.fixedID, value: (node.husb) ? node.husb.plugin.name[node.husb.plugin.name.length - 1] : "Unknown Family name" });
 
                 return currentTemplate.generateFamily(node);
 
-            /*case GedcomConst.indicator.source:
-                if(DEBUG)
-                    console.log('Es una Fuente!');
-                return currentTemplate.generateSource(node);*/
+                /*case GedcomConst.indicator.source:
+                    if(!!DEBUG)
+                        console.log('Es una Fuente!');
+                    return currentTemplate.generateSource(node);*/
         }
     }
 
@@ -114,13 +117,13 @@ window.GenManager = function() {
     this.generateHome = () => {
         let dirpath, content;
 
-        if(Preferences.session.options.noHome === true) {
+        if (Preferences.session.options.noHome === true) {
             app.ui.convert.showSuccess();
             return true;
         }
 
         dirpath = Preferences.session.dirPath;
-        content = currentTemplate.generateHome(directoryData);            
+        content = currentTemplate.generateHome(directoryData);
         fs.writeFile(dirpath + '/index.html', content, app.ui.convert.showSuccess);
     }
 
@@ -128,8 +131,8 @@ window.GenManager = function() {
         let dirpath = Preferences.session.dirPath;
 
         // Si el proceso esta en pausa no podemos continuar
-        if(Preferences.session.progress.isPaused === true) {
-            if(DEBUG) 
+        if (Preferences.session.progress.isPaused === true) {
+            if (!!DEBUG)
                 console.log('Proceso en pausa. Deteniendo.');
             return false;
         }
@@ -138,15 +141,15 @@ window.GenManager = function() {
         idxCurrentNode++;
 
         // Si el indice alcanzo el limite, generamos a Home y mostramos mensaje de exito.
-        if(idxCurrentNode > parsedKeys.length - 1) {
+        if (idxCurrentNode > parsedKeys.length - 1) {
             this.generateHome();
             return true;
         }
-        
+
         // De lo contrario, continuamos
         let currentKey = parsedKeys[idxCurrentNode];
         let currentNode = parsedData[currentKey];
-        if(DEBUG) {
+        if (!!DEBUG) {
             console.log('Nodo Actual: ' + idxCurrentNode);
         }
 
@@ -158,7 +161,7 @@ window.GenManager = function() {
 
         // Escribimos los datos
         // Hay circunstancias en donde el contenido no es generado. Si este es el caso solo pasamos al siguiente nodo.
-        if(content)
+        if (content)
             fs.writeFile(dirpath + '/' + currentNode.fixedID + '.html', content, this.generateNext);
         else
             this.generateNext();
@@ -166,15 +169,15 @@ window.GenManager = function() {
 
     this.setup = (data) => {
         parsedData = data._object;
-        parsedKeys = Object.keys(parsedData);        
-        
-        if(DEBUG) {
+        parsedKeys = Object.keys(parsedData);
+
+        if (!!DEBUG) {
             console.log('Raiz del objeto parseado')
             console.log(parsedData);
             console.log('Claves del objeto parseado');
             console.log(parsedKeys);
         }
-        
+
         Preferences.session.progress.totalNodes = parsedKeys.length;
         this.generateNext();
     }
--- src/scripts/viewer-wrapper.js
+++ src/scripts/viewer-wrapper.js
@@ -2,21 +2,28 @@
 // Viewer-Wrapper.js
 // Wrapper for Gedcom-Viewer-JS external library
 //
+var IsGedcomPatched = false;
 
 window.ViewerWrapper = function() {
+    if(!IsGedcomPatched) {
+        window.GedcomIHM = {fillQuickValues: () => null}
+        window.GedcomToolbox.show = () => null;
+        IsGedcomPatched = true;
+    }
+
     let parser = new GedcomParser();
 
     this.readFile = (fpath) => {
         return new Promise((res, rej) => {
-            if(fpath.constructor != String)
+            if (fpath.constructor != String)
                 rej(ErrCode.GEDCOM_FILE_INVALID_PARAMETER);
             fs.readFile(path.normalize(fpath), (err, data) => {
                 if (err) {
                     rej(ErrCode.GEDCOM_FILE_NOT_EXISTS)
                     return false;
                 }
-                    parser.parse(data.toString());
-                    res(parser.DATAS);
+                parser.parse(data.toString());
+                res(parser.DATAS);
             });
         });
     }
