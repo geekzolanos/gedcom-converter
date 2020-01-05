bd51a9a2e5a2f8f5451a3ad9bde7b6224236ef85
David Romero <geekzolanos@gmail.com>
2020-01-05 05:59:00 -0400

    Small fixtures to Branding updates

--- src/scripts/app-base.js
+++ src/scripts/app-base.js
@@ -6,12 +6,11 @@ const process = require('child_process');
 const fs = require('fs');
 const path = require('path');
 const electron = require('electron').remote;
-const {shell} = require('electron');
+const { shell } = require('electron');
 const ctxMenu = require('electron-contextmenu-middleware');
 const inputCtxMenu = require('electron-input-menu');
 const APP_START_PAGE = "selector";
-const UW_URL = 'http://www.upwork.com';
-const GK_URL = 'http://geekzolanos.wordpress.com';
+const GH_URL = 'https://www.github.com/geekzolanos/gedcom-converter/';
 
 //
 // Namespace APP
